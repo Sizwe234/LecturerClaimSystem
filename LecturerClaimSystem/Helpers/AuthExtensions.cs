f7a2003096eb0a3a0fd1dd2e514abf17899e7fd8
Sizwe234 <majolanation28@gmail.com>
2025-11-21 00:55:54 +0200

    Part 3 Enhancements: 7. Adding a session helper to do role checks and the state of the login as well

---
+++ LecturerClaimSystem/Helpers/AuthExtensions.cs
@@ -0,0 +1,17 @@
+﻿// Helpers/AuthExtensions.cs
+using Microsoft.AspNetCore.Http;
+
+namespace LecturerClaimSystem.Helpers
+{
+	public static class AuthExtensions
+	{
+		public static string? GetUserRole(this ISession session) =>
+			session.GetString("UserRole");
+
+		public static bool IsRole(this ISession session, string role) =>
+			session.GetString("UserRole") == role;
+
+		public static bool IsLoggedIn(this ISession session) =>
+			!string.IsNullOrWhiteSpace(session.GetString("UserEmail"));
+	}
+}
