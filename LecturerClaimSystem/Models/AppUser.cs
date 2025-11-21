b409fba921f8132320ffd0ca811608e27c7c30e4
Sizwe234 <majolanation28@gmail.com>
2025-11-21 10:12:27 +0200

    Part 3 Enhancements: Converting AppUser to IdentityUser with fFirstName, LastName, and HourlyRate

--- LecturerClaimSystem/Models/AppUser.cs
+++ LecturerClaimSystem/Models/AppUser.cs
@@ -1,28 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace LecturerClaimSystem.Models
 {
-	public class AppUser
+	public class AppUser : IdentityUser
 	{
-		public int Id { get; set; }
-
 		[Required, StringLength(50)]
 		public string FirstName { get; set; } = string.Empty;
 
 		[Required, StringLength(50)]
 		public string LastName { get; set; } = string.Empty;
 
-		[Required, EmailAddress, StringLength(100)]
-		public string Email { get; set; } = string.Empty;
-
-		[Required, StringLength(100)]
-		public string Password { get; set; } = string.Empty; // Simple demo; hashed in real apps
-
 		[Range(1, 100000)]
 		public decimal HourlyRate { get; set; } = 0;
-
-		[Required]
-		public UserRole Role { get; set; } = UserRole.Lecturer;
 
 		public string FullName => $"{FirstName} {LastName}";
 	}
